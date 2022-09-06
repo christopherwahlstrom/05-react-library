6d7a6abdc8ea22bce595bd182cb13586b2336065
David Andersson <david.andersson@zocom.se>
2022-09-06 13:53:46 +0200

    Static HTML+CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<div className="app">
-			Clear!
+		<div id="app">
+			<header>
+				Bibblan
+			</header>
+			<main>
+				<h1> Våra böcker </h1>
+				<div className="book-grid">
+					<div className="card">
+						<h3> Bokens titel </h3>
+						<img alt="bild" />
+						<p> Författare </p>
+						<button> Låna </button>
+					</div>
+					<div className="card">
+						<h3> Bokens titel </h3>
+						<img alt="bild" />
+						<p> Författare </p>
+						<button className="return"> Återlämna </button>
+					</div>
+					<div className="card">
+						<h3> Bokens titel </h3>
+						<img alt="bild" />
+						<p> Författare </p>
+						<button> Låna </button>
+					</div>
+					<div className="card">
+						<h3> Bokens titel </h3>
+						<img alt="bild" />
+						<p> Författare </p>
+						<button> Låna </button>
+					</div>
+					<div className="card">
+						<h3> Bokens titel </h3>
+						<img alt="bild" />
+						<p> Författare </p>
+						<button> Låna </button>
+					</div>
+				</div>
+			</main>
+			<footer>
+				Kontakta oss
+			</footer>
 		</div>
 	)
 }
